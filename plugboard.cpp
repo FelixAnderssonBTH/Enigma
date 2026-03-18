7adc4fd6c3469438643ae4c689e181ce7ef74f20
Felix Andersson <felix02.andersson@gmail.com>
2026-03-18 15:25:41 +0100

    added some more code

--- plugboard.cpp
+++ plugboard.cpp
@@ -7,33 +7,65 @@
 
 using namespace std;
 
-class plugboard{
+map<string, string> wiring; 
 
-  private:
-      
-      map<string, string> wiring;
-
-  public:
-      
-      
-      bool controll(string x, string y, vector<string>& alphabet){
-        
-        if(x == y){
-          cout<< "Cant pair with same letter\n";
-          return false;
-        
-        }if (wiring.count(x)){
-          cout<< "cant wire" << x << "twice\n";
-          return false;
-        
-        }if (wiring.count(y)){
-          cout<< "cant wire" << y << "twice\n";
-          return false;
-        }
-        return true;
-      }
-
-
-      
-
+bool inAlphabet(const string& character) {
+    return find(alphabet.begin(), alphabet.end(), character) != alphabet.end();
 }
+
+bool controll(string& x, string& y){
+    
+    if(x == y){
+      cout<< "Cant pair with same letter\n";
+      return false;
+    
+    }if (wiring.count(x)){
+      cout<< "cant wire" << x << "twice\n";
+      return false;
+    
+    }if (wiring.count(y)){
+      cout<< "cant wire" << y << "twice\n";
+      return false;
+    }
+    if (!inAlphabet(x) || !inAlphabet(y)){
+      cout<< "Characters must be in the aphabet\n";
+      return false;
+    }
+    return true;
+  }
+
+  
+int main(){
+    ChooseLanguage();
+
+    string a, b;
+    cout << "=== PLUGBOARD WIRING ===\n";
+    cout << "Enter letter pairs to wire together.\n";
+    cout << "Type 'done' when finished.\n\n";
+
+    while (true) {
+        cout << "write pairs (eg. A B): ";
+        cin >> x >> y;
+        
+        if (x.size() != 1) {
+          cout << "you need to seperate the pairs with blankspace, and can only give one character at a time";
+          continue;
+        }
+        if (y.size() != 1) {
+          cout << "you need to seperate the pairs with blankspace, and can only give one character at a time";
+          continue;
+        }
+
+        x[0] = toupper(a[0])
+        y[0] = toupper(b[0])
+        if (controll(x,y)) {
+            wiring[a] = b;
+            wiring[b] = a;
+            cout << "Paired " << x << " <-> " << y << "\n\n";
+        }
+    }
+
+    return 0;
+}
+  
+
